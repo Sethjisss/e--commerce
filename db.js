1c5540546fbf850c9f2eeaf61a71fe95ed6d76fe
pravin <pravinp@sssbitech.com>
2023-02-21 11:28:13 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://PravinPilania:Pravindb@cluster0.etazzzi.mongodb.net/e_commerce?retryWrites=true&w=majority'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
